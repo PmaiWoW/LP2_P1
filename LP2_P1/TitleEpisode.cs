0ce466546696f98622062ad2122ed2ff14992f18
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-11-30 20:06:45 +0000

    Add TitleCrew and TitleEpisode structs

---
+++ LP2_P1/TitleEpisode.cs
@@ -0,0 +1,19 @@
+﻿namespace LP2_P1
+{
+    public struct TitleEpisode
+    {
+        public string Tconst { get; }
+        public string ParentTconst { get; }
+        public int? SeasonNumber { get; }
+        public int? EpisodeNumber { get; }
+
+        public TitleEpisode(string tconst, string parentTconst,
+            int? seasonNumber = null, int? episodeNumber = null)
+        {
+            Tconst = tconst;
+            ParentTconst = parentTconst;
+            SeasonNumber = seasonNumber;
+            EpisodeNumber = episodeNumber;
+        }
+    }
+}
