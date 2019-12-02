540d230ec5a2e4e9630ff8dcef0c572af854323b
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-12-02 22:35:59 +0000

    Add unfinished classes TitleSearchMenu and TypeSearchMenu

---
+++ LP2_P1/TitleSearchMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LP2_P1
+{
+    static class TitleSearchMenu
+    {
+
+        public static string[] Menu()
+        {
+            string[] returnAnswer;
+
+            Console.WriteLine("Search IMDb\n" +
+                              "B. Go Back\n");
+
+
+            returnAnswer = Console.ReadLine().Split(" ");
+
+            return returnAnswer;
+        }
+
+        public static void GoBack()
+        {
+
+        }
+    }
+}
